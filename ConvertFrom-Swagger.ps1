5735ad444fabba3e5aa844cff8d384177a8f5dff
Stephen <11358399+s-nix@users.noreply.github.com>
2025-11-11 07:32:38 -0500

    Refactor Set-UpperCase function and improve ConvertFrom-SwaggerGet cmdlet for better readability

--- ConvertFrom-Swagger.ps1
+++ ConvertFrom-Swagger.ps1
@@ -1,76 +1,76 @@
 
 
 function Set-UpperCase {
-  param (
-    [string]$InputString
-  )
-  $InputString = $InputString.TrimStart("/api/v1/")
-  $InputString = $InputString -replace "\/\{.*?\}", ""
-  $strings = $InputString.Split("/")
-  $result = foreach ($str in $strings) {
-    if ($str -ne "") {
-      $parts = $str.Split("-")
-      $parts | ForEach-Object {
-        if ($_.Length -gt 0) {
-          ($_.Substring(0, 1).ToUpper() + $_.Substring(1)).Trim()
+    param (
+        [string]$InputString
+    )
+    $InputString = $InputString.TrimStart("/api/v1/")
+    $InputString = $InputString -replace "\/\{.*?\}", ""
+    $strings = $InputString.Split("/")
+    $result = foreach ($str in $strings) {
+        if ($str -ne "") {
+            $parts = $str.Split("-")
+            $parts | ForEach-Object {
+                if ($_.Length -gt 0) {
+                    ($_.Substring(0, 1).ToUpper() + $_.Substring(1)).Trim()
+                }
+            }
         }
-      }
     }
-  }
-  return $result -join ""
+    return $result -join ""
 }
 
 function ConvertFrom-SwaggerGet {
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory = $true)]
-    [string]$SwaggerFilePath
-  )
-  $swaggerContent = Get-Content -Path $SwaggerFilePath -Raw
-  $swaggerJson = $swaggerContent | ConvertFrom-Json -Depth 100
-  $endpointPaths = $swaggerJson.paths.PSObject.Members.Name | Where-Object { $_ -like "/api*" }
-  foreach ($endpointPath in $endpointPaths) {
-    if (!$swaggerJson.paths.$endpointPath.get) {
-      continue
-    }
-    $objectPrimitiveType = $swaggerJson.paths.$endpointPath.get.responses.'200'.content.'application/json'.schema.type
-    $objectType = $swaggerJson.paths.$endpointPath.get.responses.'200'.content.'application/json'.schema.'$ref' | Split-Path -Leaf -ea 0
-    $objectListType = $swaggerJson.paths.$endpointPath.get.responses.'200'.content.'application/json'.schema.items.'$ref' | Split-Path -Leaf -ea 0
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$SwaggerFilePath
+    )
+    $swaggerContent = Get-Content -Path $SwaggerFilePath -Raw
+    $swaggerJson = $swaggerContent | ConvertFrom-Json -Depth 100
+    $endpointPaths = $swaggerJson.paths.PSObject.Members.Name | Where-Object { $_ -like "/api*" }
+    foreach ($endpointPath in $endpointPaths) {
+        if (!$swaggerJson.paths.$endpointPath.get) {
+            continue
+        }
+        $objectPrimitiveType = $swaggerJson.paths.$endpointPath.get.responses.'200'.content.'application/json'.schema.type
+        $objectType = $swaggerJson.paths.$endpointPath.get.responses.'200'.content.'application/json'.schema.'$ref' | Split-Path -Leaf -ea 0
+        $objectListType = $swaggerJson.paths.$endpointPath.get.responses.'200'.content.'application/json'.schema.items.'$ref' | Split-Path -Leaf -ea 0
     
-    $targetMethodName = if ($objectPrimitiveType -and -not $objectType -and -not $objectListType) {
-      Set-UpperCase -InputString $endpointPath.TrimStart("/api/v1/")
-    }
-    elseif ($objectType -and -not $objectListType) {
-      $objectType
-    }
-    elseif ($objectListType) {
-      $objectListType
-    }
-    else {
-      #Write-Error "Unable to determine target type for endpoint: $endpointPath" -ErrorAction Stop
-      Set-UpperCase -InputString $endpointPath.TrimStart("/api/v1/")
-    }
-    $targetType = if ($objectPrimitiveType -and -not $objectType -and -not $objectListType) {
-      switch ($objectPrimitiveType) {
-        "string" { "string" }
-        "integer" { "int" }
-        "boolean" { "bool" }
-        default { "object" }
-      }
-    }
-    elseif ($objectType -and -not $objectListType) {
-      $objectType
-    }
-    elseif ($objectListType) {
-      "List<$objectListType>"
-    }
-    else {
-      #Write-Error "Unable to determine target type for endpoint: $endpointPath" -ErrorAction Stop
-      "byte[]"
-    }
-    $parameters = $swaggerJson.paths.$endpointPath.get.parameters
-    $cmdletPayload =
-    @"
+        $targetMethodName = if ($objectPrimitiveType -and -not $objectType -and -not $objectListType) {
+            Set-UpperCase -InputString $endpointPath.TrimStart("/api/v1/")
+        }
+        elseif ($objectType -and -not $objectListType) {
+            $objectType
+        }
+        elseif ($objectListType) {
+            $objectListType
+        }
+        else {
+            #Write-Error "Unable to determine target type for endpoint: $endpointPath" -ErrorAction Stop
+            Set-UpperCase -InputString $endpointPath.TrimStart("/api/v1/")
+        }
+        $targetType = if ($objectPrimitiveType -and -not $objectType -and -not $objectListType) {
+            switch ($objectPrimitiveType) {
+                "string" { "string" }
+                "integer" { "int" }
+                "boolean" { "bool" }
+                default { "object" }
+            }
+        }
+        elseif ($objectType -and -not $objectListType) {
+            $objectType
+        }
+        elseif ($objectListType) {
+            "List<$objectListType>"
+        }
+        else {
+            #Write-Error "Unable to determine target type for endpoint: $endpointPath" -ErrorAction Stop
+            "byte[]"
+        }
+        $parameters = $swaggerJson.paths.$endpointPath.get.parameters
+        $cmdletPayload =
+        @"
 using System.Management.Automation;
 using PSImmyBot.Models;
 using PSImmyBot.Services;
@@ -118,9 +118,9 @@ $(foreach ($param in $parameters) {
     }
 }
 "@
-    $cmdletFilePath = "Cmdlets/Get$targetMethodName.cs"
-    $cmdletPayload | Out-File -FilePath $cmdletFilePath -Encoding UTF8
-  }
+        $cmdletFilePath = "Cmdlets/Get$targetMethodName.cs"
+        $cmdletPayload | Out-File -FilePath $cmdletFilePath -Encoding UTF8
+    }
 }
 
 ConvertFrom-SwaggerGet -SwaggerFilePath "C:\Users\stephen.nix\Downloads\immy_swagger.json"
