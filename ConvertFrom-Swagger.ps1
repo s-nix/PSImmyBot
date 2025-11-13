ce1245a798c2a20c3ec56037ace9e8536dcd9cb6
Stephen <11358399+s-nix@users.noreply.github.com>
2025-11-13 08:55:22 -0500

    Add ConvertFrom-SwaggerPut function to handle PUT requests and generate corresponding cmdlets

--- ConvertFrom-Swagger.ps1
+++ ConvertFrom-Swagger.ps1
@@ -407,7 +407,149 @@ $(if ($hasPayload -and $hasReturn) {
     }
 }
 
+function ConvertFrom-SwaggerPut {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$SwaggerFilePath
+    )
+    $swaggerContent = Get-Content -Path $SwaggerFilePath -Raw
+    $swaggerJson = $swaggerContent | ConvertFrom-Json -Depth 100
+    $endpointPaths = $swaggerJson.paths.PSObject.Members.Name | Where-Object { $_ -like "/api*" }
+    foreach ($endpointPath in $endpointPaths) {
+        if (!$swaggerJson.paths.$endpointPath.patch) {
+            continue
+        }
+
+        $payloadPrimitiveType = $swaggerJson.paths.$endpointPath.put.requestBody.content.'application/json'.schema.type
+        $payloadObjectType = $swaggerJson.paths.$endpointPath.put.requestBody.content.'application/json'.schema.'$ref' | Split-Path -Leaf -ea 0
+        $payloadObjectListType = $swaggerJson.paths.$endpointPath.put.requestBody.content.'application/json'.schema.items.'$ref' | Split-Path -Leaf -ea 0
+        $hasPayload = $payloadPrimitiveType -or $payloadObjectType -or $payloadObjectListType
+        $payloadType = if ($payloadPrimitiveType -and -not $payloadObjectType -and -not $payloadObjectListType) {
+            switch ($payloadPrimitiveType) {
+                "string" { "string" }
+                "integer" { "int" }
+                "boolean" { "bool" }
+                default { "object" }
+            }
+        }
+        elseif ($payloadObjectType -and -not $payloadObjectListType) {
+            $payloadObjectType
+        }
+        elseif ($payloadObjectListType) {
+            "List<$payloadObjectListType>"
+        }
+        else {
+            #Write-Error "Unable to determine payload type for endpoint: $endpointPath" -ErrorAction Stop
+            "object"
+        }
+        $returnPrimitiveType = $swaggerJson.paths.$endpointPath.put.responses.'200'.content.'application/json'.schema.type
+        $returnObjectType = $swaggerJson.paths.$endpointPath.put.responses.'200'.content.'application/json'.schema.'$ref' | Split-Path -Leaf -ea 0
+        $returnObjectListType = $swaggerJson.paths.$endpointPath.put.responses.'200'.content.'application/json'.schema.items.'$ref' | Split-Path -Leaf -ea 0
+        $hasReturn = $returnPrimitiveType -or $returnObjectType -or $returnObjectListType
+
+        $targetMethodName = if ($payloadPrimitiveType -and -not $payloadObjectType -and -not $payloadObjectListType) {
+            Set-UpperCase -InputString $endpointPath.TrimStart("/api/v1/")
+        }
+        elseif ($payloadObjectType -and -not $payloadObjectListType) {
+            $payloadObjectType
+        }
+        elseif ($payloadObjectListType) {
+            $payloadObjectListType
+        }
+        else {
+            #Write-Error "Unable to determine target type for endpoint: $endpointPath" -ErrorAction Stop
+            Set-UpperCase -InputString $endpointPath.TrimStart("/api/v1/")
+        }
+
+        $targetReturnType = if ($returnPrimitiveType -and -not $returnObjectType -and -not $returnObjectListType) {
+            switch ($returnPrimitiveType) {
+                "string" { "string" }
+                "integer" { "int" }
+                "boolean" { "bool" }
+                default { "object" }
+            }
+        }
+        elseif ($returnObjectType -and -not $returnObjectListType) {
+            $returnObjectType
+        }
+        elseif ($returnObjectListType) {
+            "List<$returnObjectListType>"
+        }
+        else {
+            #Write-Error "Unable to determine target return type for endpoint: $endpointPath" -ErrorAction Stop
+            "void"
+        }
+        $parameters = $swaggerJson.paths.$endpointPath.put.parameters
+        $cmdletPayload =
+        @"
+using System.Management.Automation;
+using PSImmyBot.Models;
+using PSImmyBot.Services;
+
+namespace PSImmyBot.Cmdlets;
+
+[Cmdlet("Push", "$targetMethodName")]
+public class Push$targetMethodName : Cmdlet {
+$(foreach ($param in $parameters) {
+    $paramName = $param.name
+    $paramType = $param.schema.'$ref' | Split-Path -Leaf -ea 0
+    $paramRequired = $param.required -eq $true
+    if (-not $paramType) {
+        $paramType = switch ($param.schema.type) {
+            "string" { "string" }
+            "integer" { "int" }
+            "boolean" { "bool" }
+            default { "object" }
+        }
+    }
+    $paramType = if (!$paramRequired) { "$paramType`?" } else { $paramType }
+    "    [Parameter(Mandatory = $($paramRequired.ToString().ToLower()))]`n    public $paramType $paramName { get; set; }`n`n"
+})
+$(if($hasPayload) {
+    "    [Parameter(Mandatory = true)] public $payloadType PayloadBody { get; set; }`n`n"
+})
+    protected override void ProcessRecord() {
+        string endpoint = `$"$endpointPath`?";
+$(foreach ($param in $parameters) {
+    $paramName = $param.name
+    $paramType = $param.schema.'$ref' | Split-Path -Leaf -ea 0
+    $paramRequired = $param.required -eq $true
+    if (-not $paramType) {
+        $paramType = switch ($param.schema.type) {
+            "string" { "string" }
+            "integer" { "int" }
+            "boolean" { "bool" }
+            default { "object" }
+        }
+    }
+    $paramType = if (!$paramRequired) { "$paramType`?" } else { $paramType }
+    if ($param.in -eq "query") {
+        "        endpoint += Globals.ConvertToQueryParameters($paramName);`n"
+    }
+})
+$(if ($hasPayload -and $hasReturn) {
+    "        $targetReturnType response = ImmyBotApiService.Put<$payloadType, $targetReturnType>(endpoint.TrimEnd('?').TrimEnd('&'), PayloadBody).GetAwaiter().GetResult();
+        WriteObject(response);"
+} elseif ($hasPayload -and -not $hasReturn) {
+    "        ImmyBotApiService.Put(endpoint.TrimEnd('?').TrimEnd('&'), PayloadBody).GetAwaiter().GetResult();"
+} elseif (-not $hasPayload -and $hasReturn) {
+    "        $targetReturnType response = ImmyBotApiService.Put<$targetReturnType>(endpoint.TrimEnd('?').TrimEnd('&')).GetAwaiter().GetResult();
+        WriteObject(response);"
+} else {
+    "        ImmyBotApiService.Put(endpoint.TrimEnd('?').TrimEnd('&')).GetAwaiter().GetResult();"
+})
+    }
+
+}
+"@
+        $cmdletFilePath = "Cmdlets/Push$targetMethodName.cs"
+        $cmdletPayload | Out-File -FilePath $cmdletFilePath -Encoding UTF8
+    }
+}
+
 
 # ConvertFrom-SwaggerGet -SwaggerFilePath "C:\Users\stephen.nix\Downloads\immy_swagger.json"
 # ConvertFrom-SwaggerPost -SwaggerFilePath "C:\Users\stephen.nix\Downloads\immy_swagger.json"
-ConvertFrom-SwaggerPatch -SwaggerFilePath "C:\Users\stephen.nix\Downloads\immy_swagger.json"
+# ConvertFrom-SwaggerPatch -SwaggerFilePath "C:\Users\stephen.nix\Downloads\immy_swagger.json"
+ConvertFrom-SwaggerPut -SwaggerFilePath "C:\Users\stephen.nix\Downloads\immy_swagger.json"
